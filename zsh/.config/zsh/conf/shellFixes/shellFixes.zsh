025d0e3f230db2466476fa3ead75b2cbf20c3ec1
nazdridoy <nazdridoy399@gmail.com>
2025-07-09 03:52:34 +0600

    fix(zsh): adjust ARGV0 unset condition for Cursor AppImage
    - [fix] Change ARGV0 unset condition from exact path match to pattern match (zsh/.config/zsh/conf/shellFixes/shellFixes.zsh:3)

--- zsh/.config/zsh/conf/shellFixes/shellFixes.zsh
+++ zsh/.config/zsh/conf/shellFixes/shellFixes.zsh
@@ -1,5 +1,5 @@
 ## Fix Cursor Terminal ZSH ENV issue
 
-if [[ "$ARGV0" == "/opt/cursor-bin/cursor-bin.AppImage" ]]; then
+if [[ "$ARGV0" == *cursor* && "$ARGV0" == *AppImage* ]]; then
   unset ARGV0
 fi
